0a2eeb1ac6528c90278ca3fe973077912408109a
Yago Barbosa <yagosbarbosa26@gmail.com>
2026-01-15 13:18:50 -0300

    #

--- Course/Funcoes.cs
+++ Course/Funcoes.cs
@@ -8,7 +8,25 @@ namespace Course
     {
         public static void Func()
         {
+            Console.WriteLine("Digite 3 números um de cada vez: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
 
+            double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("O Maior é " + resultado);
+        }
+
+        static int Maior(int a, int b, int c){
+            int m;
+            if(a > b && a > c)
+                m = a;
+            else if (b > c)
+                m = b;
+            else
+                m = c;
+            return m;
         }
     }
 }
