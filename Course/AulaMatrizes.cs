df09f79200381fc408ba05a7299ecf2c6d39eab3
Yago Barbosa <yagosbarbosa26@gmail.com>
2026-02-05 12:08:15 -0300

    Aula 80 - Matrizes

---
+++ Course/AulaMatrizes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course
+{
+    internal class AulaMatrizes
+    {
+        public static void Aula()
+        {
+
+            //tipo de matriz, [,] , nome e se necessario sua inicialização  
+            double[,] mat = new double[2, 3];
+
+            // Length diz quantos elementos a matriz tem no total
+            Console.WriteLine(mat.Length);
+
+            // Rank diz quanto é a primeira dimensão da matriz (quantidade de linhas)
+            Console.WriteLine(mat.Rank);
+
+            // GetLength pode se especifcar em qual dimensão deseja visualizar seu tamanho(Linhas)
+            Console.WriteLine(mat.GetLength(0));
+
+            // Colunas
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
