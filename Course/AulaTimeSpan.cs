70ffe39b4fc524f06c2c89a833a396132eb2a0ca
Yago Barbosa <yagosbarbosa26@gmail.com>
2026-02-12 10:05:46 -0300

    Aula 93 - Propriedades e operações com TimeSpan

--- Course/AulaTimeSpan.cs
+++ Course/AulaTimeSpan.cs
@@ -6,6 +6,28 @@ namespace Course
 {
     internal class AulaTimeSpan
     {
+        public static void AulaFuncoesExtra()
+        {
+            TimeSpan t1 = new TimeSpan(1, 30, 10);
+            TimeSpan t2 = new TimeSpan(0, 10, 5);
+
+            TimeSpan sum = t1.Add(t2);
+            TimeSpan dif = t1.Subtract(t2);
+            TimeSpan mult = t2.Multiply(2.0);
+            TimeSpan div = t2.Divide(2);
+
+            Console.WriteLine(sum);
+            Console.WriteLine(dif);
+            Console.WriteLine(mult);
+            Console.WriteLine(div);
+            //TimeSpan t1 = new TimeSpan(2, 3, 5, 7, 11);
+
+            //Console.WriteLine(t1);
+            //// Days devolve a quantidade inteira de dias
+            //Console.WriteLine("Days: " + t1.Days);
+            //// TotalDays devolve a quantidade total de dias, mesmo que seja uma fração
+            //Console.WriteLine("TotalDays: " + t1.TotalDays);
+        }
         public static void Aula()
         {
             TimeSpan t1 = TimeSpan.FromDays(1.5);
