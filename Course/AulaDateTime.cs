f8c9b19ed3e3b1394a75145989ad25d408b370c7
Yago Barbosa <yagosbarbosa26@gmail.com>
2026-02-12 10:26:49 -0300

    Aula 94 - DateTimeKind e padrão ISO 8601

--- Course/AulaDateTime.cs
+++ Course/AulaDateTime.cs
@@ -7,6 +7,48 @@ namespace Course
 {
     internal class AulaDateTime
     {
+        public static void AulaBoasPraticas()
+        {
+            DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            // Padrao ISO 8601 - já instacia com o Kind Local
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z");
+
+            Console.WriteLine("d1: " + d1);
+            Console.WriteLine("d1 Kind: " + d1.Kind);
+            Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+
+            Console.WriteLine();
+            Console.WriteLine("d2: " + d2);
+            Console.WriteLine("d2 Kind: " + d2.Kind);
+            Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+
+            Console.WriteLine();
+            Console.WriteLine(d2.ToString("yyyy-MM-ddTHH:mm:ssZ"));// Cuidado - Primeiro converta para formato universal
+            Console.WriteLine(d2.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+            //DateTime d1 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Local);
+            //DateTime d2 = new DateTime(2000, 8, 15, 13, 5, 58, DateTimeKind.Utc);
+            //DateTime d3 = new DateTime(2000, 8, 15, 13, 5, 58);
+
+            //Console.WriteLine("d1: " + d1);
+            //Console.WriteLine("d1 Kind: " + d1.Kind);
+            //Console.WriteLine("d1 to Local: " + d1.ToLocalTime());
+            //Console.WriteLine("d1 to Utc: " + d1.ToUniversalTime());
+
+            //Console.WriteLine();
+            //Console.WriteLine("d2: " + d2);
+            //Console.WriteLine("d2 Kind: " + d2.Kind);
+            //Console.WriteLine("d2 to Local: " + d2.ToLocalTime());
+            //Console.WriteLine("d2 to Utc: " + d2.ToUniversalTime());
+
+            //Console.WriteLine();
+            //Console.WriteLine("d3: " + d3);
+            //Console.WriteLine("d3 Kind: " + d3.Kind);
+            //Console.WriteLine("d3 to Local: " + d3.ToLocalTime());
+            //Console.WriteLine("d3 to Utc: " + d3.ToUniversalTime());
+        }
         public static void AulaFuncoesExtra()
         {
             DateTime d1 = new DateTime(2000, 10, 15);
@@ -40,6 +82,7 @@ namespace Course
             //Console.WriteLine(s6);
             //Console.WriteLine(s7);
         }
+
         public static void AulaParse()
         {
             DateTime d1 = DateTime.Parse("2000-08-15");
--- Course/Testes.cs
+++ Course/Testes.cs
@@ -8,7 +8,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            AulaTimeSpan.AulaFuncoesExtra();
+            AulaDateTime.AulaBoasPraticas();
             //Funcoes.Func();
         }
         static void test(string[] args)
