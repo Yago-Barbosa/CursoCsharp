0a1584d5366264be7792e4ea15c25e42a2f7da8b
Yago Barbosa <yagosbarbosa26@gmail.com>
2026-03-20 10:43:25 -0300

    Aulas 132 e 133 - FileStream, StreamReader e Using

--- Course/Aulas/AulaFiles.cs
+++ Course/Aulas/AulaFiles.cs
@@ -7,7 +7,63 @@ namespace Course.Aulas
 {
     internal class AulaFiles
     {
-        public static void Pratica()
+        public static void BlockUsing()
+        {
+
+            string path = @"c:\temp\file1.txt";
+
+            // Ao usar esse bloco todos os recursos instanciados nele serão fechados ao final da execução
+            // Também é possivel "cascatear" esse bloco
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void PraticaFileStream()
+        {
+            string path = @"c:\temp\file1.txt";
+
+            // FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                // fs = new FileStream(path, FileMode.Open);
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine()!;
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // O filestream e streamreader são recursos externos e devem ser fechados (maneira manual)
+                if (sr != null) sr.Close();
+                // if( fs != null ) fs.Close();
+            }
+        }
+
+        public static void PraticaFile()
         {
             string sourcePath = @"c:\temp\file1.txt";
             string targetPath = @"c:\temp\file2.txt";
--- Course/Testes.cs
+++ Course/Testes.cs
@@ -12,7 +12,7 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Aulas.AulaFiles.Pratica();
+            Aulas.AulaFiles.BlockUsing();
             //Projetos.ProjetoCalculadora.Calculadora.Calc();
             //Adivinhe.Jogo();
             //Funcoes.Func();
