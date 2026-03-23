65fd01699c5cb4935c1a108db83d0f40eb0dff44
Yago Barbosa <yagosbarbosa26@gmail.com>
2026-03-23 11:46:21 -0300

    Aula 140 - Solução sem Interface Parte 1

---
+++ Course/Aulas/AulaInterface.cs
@@ -0,0 +1,23 @@
+﻿using Course.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Course.Aulas
+{
+    internal class AulaInterface
+    {
+        public static void SemInterface()
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine()!;
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine()!, "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine()!, "dd/MM/yyyy hh:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+        }
+    }
+}
